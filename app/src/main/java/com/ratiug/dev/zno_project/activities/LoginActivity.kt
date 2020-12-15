971a02e7d55b51f8e0953d710ede906cfe0ea714
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-12-15 20:16:32 +0200

    create portrait screen activity_login.xml

---
+++ app/src/main/java/com/ratiug/dev/zno_project/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.ratiug.dev.zno_project.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ratiug.dev.zno_project.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
